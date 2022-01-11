d697ac6d68a1385b1745edb9b405645482262150
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2022-01-11 14:30:39 +0100

    ok

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,9 +1,69 @@
+import {useState} from 'react';
+
+const App = () => {
+  //let [isLandingPage, setMainDisplay] = useState(true);
+  let [isLandingPage, setterF] = useState(true);
+  
+  /*
+  const toogleDisplay = () => {
+    setterF(!isLandingPage);
+    //setMainDisplay((prevState) => !prevState);
+  }
+  */
+  
+  return (
+    <div>
+      <p>Hello Word</p>
+      <div>
+        {/*<Page/>*/}
+        {/*Page()*/}
+        {
+          isLandingPage ? 
+          <div>Landing Page</div> : 
+          <div>Homepage</div>
+        }
+      </div>
+      <br/>
+      <button onClick={() => setterF(!isLandingPage)}>Click</button>
+    </div>
+  )
+}
+
+/*
+const Page = () => {
+  if (bool){
+    return <div>Homepage</div>
+  }
+  else {
+    return <div>Landing Page</div>
+  }
+}
+*/
+/*
+const handleClick = () => {
+  console.log(bool + ' volt');
+  bool = !bool;
+  console.log(bool + ' lett');
+}
+
+let bool = false;
 const App = () => {
   return (
     <div>
-      
+    <p>Hello Word</p>
+    <div>
+        {
+        bool ? 
+          <div>Homegpage</div> : 
+          <div>Landingpage</div>
+        }
+        {
+      </div>
+      <br/>
+      <button onClick={handleClick}>Click</button>
     </div>
-  );
+  )
 }
+*/
 
 export default App;
