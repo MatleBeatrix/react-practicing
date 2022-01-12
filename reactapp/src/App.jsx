3740a812e537ebe44209dcd0c32965dc79e9522a
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2022-01-12 15:18:43 +0100

    ok

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,40 +1,58 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-const App = () => {
-  let [isLandingPage, setterF] = useState(true);
-  let [pageName, setPageName] = useState('home');
+//const inputValue = "Haho";
 
-  //let shouldShowHomepage = pageName === 'about';  //igaz vagy hamis értéket add vissza
-  
-  const toogleDisplay = (text) => {
-    setterF(!isLandingPage);
-    console.log(text);
+const cars = [
+  {brand: "Toyota", model: "yaris", topSpeed: "190km/h"},
+  {brand: "Mercedes", model: "C63", topSpeed: "260km/h"},
+  {brand: "Mazda", model: "6", topSpeed: "210km/h"},
+  {brand: "Mazda", model: "Civic", topSpeed: "200km/h"},
+  {brand: "Audi", model: "A4", topSpeed: "220km/h"},
+]
+
+const listHandle = () => {
+  const newArray = []
+  for (const car of cars) {
+    
+    newArray.push(<p key={car.model}>{car.brand} - {car.model} ({car.topSpeed})</p>)
   }
+  return newArray;
+}
+
+
+
+
+const App = () => {  
+  let [inputValue2, setValue] = useState("Szoveg");
+  /*
+  const addButton = () => {
+      if (inputValue2.length <3){
+        console.log(inputValue2);
+        return (<div><button disabled>Submit</button><p>Min 3 charakter</p></div>);
+      }
+      return (<div><button>Submit</button></div>);
+    
+  }
+*/
   
-  
+
   return (
     <div>
       <p>Hello Word</p>
-      <div>
-        {
-          isLandingPage ? 
-          <div>Landing Page</div> : 
-          <div>Homepage</div>
-        }
-      </div>
-      <button onClick={() => setterF(!isLandingPage)}>Click</button>
-      <button onClick={toogleDisplay}>Click</button>
-      <hr/>
-      <button onClick={()=> {setPageName("home")}}>Home</button>
-      <button onClick={()=> {setPageName("about")}}>About</button>
-      <button onClick={()=> {setPageName("landing")}}>Landing</button>
-      <br/><br/>
-      <div className='content'>
-        {pageName === "home" && <div>Content of Homepage<h1>Welcome to the Homepage!</h1></div>}
-        {pageName === "about" && <div>Content of About Page<h2>This page is very cool</h2></div>}
-        {pageName === "landing" && <div>Content of Landing Page <br/><br/><input type="text" placeholder='type'/></div>}
-      </div>
-      <hr/>
+      <input type="text" value={inputValue2} onChange={(event) => {setValue(event.target.value)}}/>
+
+      {/*addButton()*/}
+      <button disabled={inputValue2.length<3}>Submit</button>
+      {inputValue2.length <3 && <p>Min 3 char</p>}
+      {/*
+      {inputValue2.length >=3 && <button>Submit</button>}
+      {inputValue2.length <3 && <button disabled>Submit</button>}
+      {inputValue2.length <3 && <p>Min. 3 char</p>}
+      */}
+      {<hr/>}
+      {cars.filter(car => car.brand.startsWith(inputValue2)).map((car) => { 
+          return <div key={car.model}>{car.brand} - {car.model} ({car.topSpeed})</div> 
+      })}
     </div>
   )
 }
